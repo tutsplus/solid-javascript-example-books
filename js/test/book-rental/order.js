c17bca31e56a14ffe440d07e927fbf3c1724afd8
José Mota <jose@josemota.net>
2013-07-19 17:22:53 +0100

    Liskov Substitution Principle.
    Swap Order for FlexibleOrder to assert that the principle is in effect.

--- js/test/book-rental/order.js
+++ js/test/book-rental/order.js
@@ -4,20 +4,27 @@ describe("Book rental", function() {
     var book = new Book("Lord of the Rings");
 
     it("belongs to a user", function() {
-      expect(new Order(user).user).toBe(user);
+      expect(new FlexibleOrder(user).user).toBe(user);
     });
 
     it("can have books", function() {
-      var order = new Order(user);
+      var order = new FlexibleOrder(user);
       order.addBook(book);
       expect(order.items[0].book).toBe(book);
+    });
+
+    it("can be terminated", function() {
+      var order = new FlexibleOrder(user);
+      order.addBook(book);
+      order.terminate(book);
+      expect(order.status).toBe("complete");
     });
 
     describe("with two books with different day limits", function() {
       var second_book = new Book("Harry Potter 1", 3);
 
       it("sets the appropriate day limit", function() {
-        var order = new Order(user);
+        var order = new FlexibleOrder(user);
         order.addBook(book);
         order.addBook(second_book);
 
